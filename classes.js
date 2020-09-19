14f8a7879d222d5bb5b60c7e3268a34cd833ab20
Jayden Gray <graydenjay@me.com>
2020-09-19 17:14:58 +1200

    Change colours of circles and make square visible

--- classes.js
+++ classes.js
@@ -5,9 +5,11 @@ document.addEventListener('DOMContentLoaded', start)
 
 function start () {
   // The first example is done for you. Uncomment the line below and reload the browser.
-  // one()
-    
+  one()
   // Your turn! Create a new function called `two`, then call it from here.
+  two()  
+  three()
+  makeVisible()
 }
 
 function one () {
@@ -18,8 +20,17 @@ function one () {
   one.classList.add('blue')
 }
 
-// CREATE FUNCTION two HERE
+function two() {
+  var two = document.getElementById('two')
+  two.classList.add('green')
+}
 
-// CREATE FUNCTION three HERE
+function three() {
+  var three = document.getElementById('three')
+  three.classList.add('purple')
+}
 
-// CREATE FUNCTION makeVisible HERE
+function makeVisible() {
+  var makeVisible = document.getElementsByClassName('invisible')[0]
+  makeVisible.classList.add('visible')
+}
